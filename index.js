93e651b6bca7487f8ff3b39286ab9c2c2b96e13c
李国辉的电脑 <liguohui@liguohuidediannaodeMacBook-Pro.local>
2019-05-15 20:45:40 +0800

    升级react-router至V5

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
-'use strict';
+"use strict";
 
 const Provider = require('react-redux').Provider;
+
 const React = require('react');
+
 const reactRouterDom = require('react-router-dom');
+
 const StaticRouter = reactRouterDom.StaticRouter;
 const Route = reactRouterDom.Route;
 
 const reactRouterConfig = require('react-router-config');
-const renderRoutes = reactRouterConfig.renderRoutes;
-const renderToString = require('react-dom/server').renderToString;
-// import createHistory from 'history/createMemoryHistory';
-var _createMemoryHistory = require('history/createMemoryHistory');
 
-var createHistory = _interopRequireDefault(_createMemoryHistory).default;
+const renderRoutes = reactRouterConfig.renderRoutes;
+
+const renderToString = require('react-dom/server').renderToString; // import createHistory from 'history/createMemoryHistory';
+
+
+const _createMemoryHistory = require('history/createMemoryHistory');
+
+const createHistory = _interopRequireDefault(_createMemoryHistory).default;
 
 function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : { default: obj };
+  return obj && obj.__esModule ? obj : {
+    default: obj
+  };
 }
 
-let SSR = ({ctx, routesconfig, configureStore, initialState}, cb) => {
-    // let path = (ctx.path === '/' + appName) ? '/' : ctx.path.replace('/' + appName, '');
-    let path = ctx.path;
-    let history = createHistory();
-    let store = configureStore(initialState, history);
-    let context = {};
+const SSR = ({
+  ctx,
+  routesconfig,
+  configureStore,
+  initialState
+}, cb) => {
+  // let path = (ctx.path === '/' + appName) ? '/' : ctx.path.replace('/' + appName, '');
+  let path = ctx.path;
+  let history = createHistory();
+  let store = configureStore(initialState, history);
+  let context = {};
+  let renderHtml = renderToString(React.createElement(Provider, {
+    store: store
+  }, React.createElement(StaticRouter, {
+    location: {
+      pathname: path
+    },
+    context: context
+  }, renderRoutes(routesconfig))));
 
-    let renderHtml = renderToString(React.createElement(
-        Provider,
-        { store: store },
-        React.createElement(
-            StaticRouter,
-            {
-                location: { pathname: path },
-                context: context },
-            renderRoutes(routesconfig)
-        )
-    ));
-    if (context.status === 404) {
-        // 获取状态码并响应；
-        ctx.status = 404;
-        ctx.body = '404 NOT FOUND';
-    } else if (context.status === 302) {
-        // 获取状态码并响应；
-        ctx.status = 302;
-        ctx.redirect(context.url);
-    }
-    cb(renderHtml);
+  if (context.status === 404) {
+    // 获取状态码并响应；
+    ctx.status = 404;
+    ctx.body = '404 NOT FOUND';
+  } else if (context.status === 302) {
+    // 获取状态码并响应；
+    ctx.status = 302;
+    ctx.redirect(context.url);
+  }
+
+  cb(renderHtml);
 };
+
 module.exports = SSR;
